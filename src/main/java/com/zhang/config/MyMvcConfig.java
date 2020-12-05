793060ea1f0644bc617c57fdb8e3b820b26f0008
WanderZhang <72598857+WandererZhang@users.noreply.github.com>
2020-12-05 18:23:36 +0800

    Update MyMvcConfig.java

--- src/main/java/com/zhang/config/MyMvcConfig.java
+++ src/main/java/com/zhang/config/MyMvcConfig.java
@@ -16,24 +16,9 @@ public class MyMvcConfig implements WebMvcConfigurer {
 
         //管理员模块视图
         registry.addViewController("/admin").setViewName("/admin/indexAdmin");
-//        registry.addViewController("/admin/showStudent").setViewName("/admin/showStudent");
-//        registry.addViewController("/admin/addStudent").setViewName("/admin/addStudent");
-//        registry.addViewController("/admin/updateStudent").setViewName("/admin/updateStudent");
-//        registry.addViewController("/admin/showUser").setViewName("/admin/showUser");
-//        registry.addViewController("/admin/addUser").setViewName("/admin/addUser");
-//        registry.addViewController("/admin/updateUser").setViewName("/admin/updateUser");
-//        registry.addViewController("/admin/showTeacher").setViewName("/admin/showTeacher");
-//        registry.addViewController("/admin/addTeacher").setViewName("/admin/addTeacher");
-//        registry.addViewController("/admin/updateTeacher").setViewName("/admin/updateTeacher");
-//        registry.addViewController("/admin/showCourse").setViewName("/admin/showCourse");
-//        registry.addViewController("/admin/addCourse").setViewName("/admin/addCourse");
-//        registry.addViewController("/admin/updateCourse").setViewName("/admin/updateCourse");
 
         //教师模块视图
         registry.addViewController("/teacher").setViewName("/teacher/indexTeacher");
-//        registry.addViewController("/teacher/showCourse").setViewName("/teacher/showCourse");
-//        registry.addViewController("/teacher/addCourse").setViewName("/teacher/addCourse");
-//        registry.addViewController("/teacher/updateCourse").setViewName("/teacher/updateCourse");
 
         //学生模块视图
         registry.addViewController("/student").setViewName("/student/indexStudent");
